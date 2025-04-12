097b36714fb720c2f19c307b63e5a80fc18d94c2
docherryra <docherryra@gmail.com>
2025-04-12 21:06:59 +0900

    init

---
+++ src/main/java/ooad/tolp/TolpApplication.java
@@ -0,0 +1,13 @@
+package ooad.tolp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TolpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TolpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ooad/tolp/TolpApplicationTests.java
@@ -0,0 +1,13 @@
+package ooad.tolp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TolpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
