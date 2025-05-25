c327bece0a975789e0231bd09293e1e70bf3e564
docherryra <docherryra@gmail.com>
2025-05-25 17:48:21 +0900

    feat: lectureVideo 업로드/삭제/조회 api 구현

--- src/main/java/ooad/tolp/lecture/dto/LectureVideoResponse.java
+++ src/main/java/ooad/tolp/lecture/dto/LectureVideoResponse.java
@@ -1,15 +1,23 @@
 package ooad.tolp.lecture.dto;
 
 import lombok.*;
+import ooad.tolp.lecture.domain.LectureVideo;
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor @Builder
 public class LectureVideoResponse {
     private Long id;
     private String title;
     private String videoUrl;
     private LocalDateTime postedAt;
+
+    public static LectureVideoResponse fromEntity(LectureVideo video) {
+        return LectureVideoResponse.builder()
+                .id(video.getId())
+                .title(video.getTitle())
+                .videoUrl(video.getVideoUrl())
+                .postedAt(video.getPostedAt())
+                .build();
+    }
 }
